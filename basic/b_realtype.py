92c533b145e4ffecadf82b09906e5e2cae3f5c7b
yeonsuan <sweetsuan@naver.com>
2021-07-16 15:00:33 +0900

    파이썬 문법

---
+++ basic/b_realtype.py
@@ -0,0 +1,32 @@
+# 자료형 - 실수형
+a = 157.93
+print(a)
+
+a=-1837.2
+print(a)
+
+a=5.
+print(a)
+
+a=-.7
+print(a)
+
+
+#10억의 지수표현방식
+a=1e9
+print(a)
+
+#752.5
+a=75.25e1
+print(a)
+
+a=3954e-3
+print(a)
+
+a = 0.3+0.6
+print(a)
+
+if a == 0.9: 
+    print(True)
+else:
+    print(False)
